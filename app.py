13fb9a4a44f96d2394b64c2da92f34e21bb15ec9
Shakil <shakil040404@gmail.com>
2025-05-07 16:59:01 +0600

    Your commit message

--- app.py
+++ app.py
@@ -10,5 +10,6 @@ def index():
     return render_template('index.html', emails=emails, bodies=bodies)
 
 if __name__ == "__main__":
+    app.run(debug=True)
     port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port)
